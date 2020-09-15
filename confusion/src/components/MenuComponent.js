57be98ac34c83ad5b2484ba647f76cf0563af395
baibhav1314 <baibhav.p1314@gmail.com>
2020-09-15 20:17:38 +0530

    SPA Part 2

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,15 +1,18 @@
 import React from 'react';
 // import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+   CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-function RenderMenuItem ({dish, onClick}) {
+function RenderMenuItem ({dish}) {
    return(
-      <Card key={dish.id} onClick={() => onClick(dish.id)}>
-         <CardImg width="100%" src={dish.image} alt={dish.name} />
-         <CardImgOverlay>
-               <CardTitle>{dish.name}</CardTitle>
-         </CardImgOverlay>
+      <Card key={dish.id} >
+         <Link to={`/menu/${dish.id}`} >
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                  <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+         </Link>
       </Card>
    );
 }
@@ -27,7 +30,7 @@ function RenderMenuItem ({dish, onClick}) {
                      <p>{dish.description}</p>
                   </Media>
                </Media>  */}
-               <RenderMenuItem dish={dish} onClick={props.onClick} />
+               <RenderMenuItem dish={dish} />
             </div>
          );
       });
@@ -35,6 +38,16 @@ function RenderMenuItem ({dish, onClick}) {
 
       return(
          <div className="container">
+            <div className="row">
+               <Breadcrumb>
+                  <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                  <BreadcrumbItem active>Menu</BreadcrumbItem>
+               </Breadcrumb>
+               <div className="col-12">
+                  <h3>Menu</h3>
+                  <hr />
+               </div>                
+            </div>
             <div className="row">
                {/* <Media list>
                   {menu}
